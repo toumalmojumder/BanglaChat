5ab87a73673cad40efbf7767845e38bd8739cea7
Toumal Mojumder <32560686+toumalmojumder@users.noreply.github.com>
2019-12-14 03:29:20 +0600

    Initial commit

---
+++ app/src/main/java/com/toumal/banglachat/MainActivity.java
@@ -0,0 +1,53 @@
+package com.toumal.banglachat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabsAccessorAdapter;
+
+    private FirebaseUser currentUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Bangla Chat");//Add Title in this code...
+
+        myViewPager = findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+
+        myTabLayout = findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(currentUser == null){
+            SendUserToLoginActivity();
+        }
+
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(loginIntent);
+    }
+}
