a1614727dcc489c90167f4e7b79f7039c9fae325
Toumal Mojumder <32560686+toumalmojumder@users.noreply.github.com>
2019-12-14 05:42:41 +0600

    registration done...

--- app/src/main/java/com/toumal/banglachat/LoginActivity.java
+++ app/src/main/java/com/toumal/banglachat/LoginActivity.java
@@ -4,16 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseUser currentUser;
+    private Button loginButton;
+    private EditText userEmail,userPassword;
+    private TextView needNewAccountLink, forgetPasswordLink;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton = findViewById(R.id.login_button);
+        userEmail = findViewById(R.id.login_email);
+        userPassword = findViewById(R.id.login_password);
+        needNewAccountLink = findViewById(R.id.need_new_account);
+        forgetPasswordLink = findViewById(R.id.forgot_password_link);
+
+        needNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToRegisterActivity();
+            }
+        });
     }
+
+    private void SendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(registerIntent);
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
--- app/src/main/java/com/toumal/banglachat/RegisterActivity.java
+++ app/src/main/java/com/toumal/banglachat/RegisterActivity.java
@@ -1,14 +1,101 @@
 package com.toumal.banglachat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button createAccountButton;
+    private EditText userEmail,userPassword;
+    private TextView alreadyHaveAccountLink;
+
+    private FirebaseAuth myAuth;
+
+    private ProgressDialog loadingBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        myAuth = FirebaseAuth.getInstance();
+
+        createAccountButton = findViewById(R.id.register_button);
+        userEmail = findViewById(R.id.register_email);
+        userPassword = findViewById(R.id.register_password);
+        alreadyHaveAccountLink = findViewById(R.id.already_have_account_link);
+        loadingBar = new ProgressDialog(this);
+
+        alreadyHaveAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToLoginActivity();
+            }
+        });
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              CreateNewAccount();
+            }
+        });
+    }
+
+    private void CreateNewAccount(){
+        String email = userEmail.getText().toString();
+        String password = userPassword.getText().toString();
+
+        if(email.isEmpty()){
+            Toast.makeText(this, "Please enter Email",Toast.LENGTH_LONG).show();
+        }
+        if(password.isEmpty()){
+            Toast.makeText(this, "Please enter Password",Toast.LENGTH_LONG).show();
+        }
+
+        else{
+
+            loadingBar.setTitle("Creating new Account");
+            loadingBar.setMessage("Please wait, while We are Creating new account for you...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            myAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "Account Created Successfully...",Toast.LENGTH_LONG).show();
+                        loadingBar.dismiss();
+                        SendUserToLoginActivity();
+                    }
+                    else{
+                        String message = task.getException().toString();
+                        Toast.makeText(RegisterActivity.this, "Error : "+message,Toast.LENGTH_LONG).show();
+                        loadingBar.dismiss();
+                    }
+
+                }
+            });
+        }
+    }
+    private void SendUserToLoginActivity() {
+        Intent registerIntent = new Intent(RegisterActivity.this,LoginActivity.class);
+        startActivity(registerIntent);
     }
 }
